4f7b461738abbf5c82f03f2ee5afd6871cf9a3f7
Eliana <elianacristaldoo@gmail.com>
2022-06-12 15:56:32 -0300

    New ask

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 //         }if(this.precio >=5000){
 //             alert(`Su libro tiene un descuento del 30% precio final es: `+ Math.round(this.precio/(1.3)));
 //         }
-//     }
+//   
 // }
 
 // const it= new Libro(`It`, 6400);
@@ -42,35 +42,34 @@
 
 
 
-
-//  var libros = new Array;
-//  libros = [
-//     [`It`, `$6.400`],
-//     [`Después`, `$4.200`], 
-//     [`El Resplandor`, `$3.600`], 
-//     [`La Cúpula`, `$6.500`],
-//     [`La Expedición`, `$3.600`],
-//     [`Ojos de fuego`, `$4.200`],
-//     [`Desesperación`, `$3.700`],
-//     [`El Visitante`, `$4.700`],
-//     [`la milla verde`, `$3.500`],
-//     [`Después de medianoche`, `$2.800`],
-//     [`Cementerio de animales`, `$3.500`],
-//     [`El bazar de los malos sueños`, `$3.500`],
-//     [`Quien pierde, paga`, `$3.000`],
-//     [`La tormenta del siglo`, `$4.000`],
-//     [`Rabia`, `$2.500`],
-//     [`Misery`, `$3.000`],
-//     [`Joyland`, `$3.000`],
-//     [`La mitad oscura`, `$4.500`],
-//     [`El instituto`, `$3.600`],
-//     [`Christine`, `$2.000`]
-//  ];
-// libros.sort( function(v1, v2){return v1[1]>v2[1]?-1:1});        
-// console.table(libros);
-
-//let nombreL = prompt("Escribe el nombre del libro")//.toUpperCase();
-// console.log(libros.includes["Rabia"])
-
-//¿cómo puedo buscar una palabra especifica siendo un array dentro de otro?
+  var libros = new Array;
+  libros = [
+     {nombre: `It`, precio: 6400},
+     {nombre: `Después`, precio: 4200}, 
+     {nombre: `El Resplandor`, precio: 3600}, 
+     {nombre: `La Cúpula`, precio: 6500},
+     {nombre: `La Expedición`, precio: 3600},
+     {nombre: `Ojos de fuego`, precio: 4200},
+     {nombre: `Desesperación`, precio: 3700},
+     {nombre: `El Visitante`, precio: 4700},
+     {nombre: `la milla verde`, precio: 3500},
+     {nombre: `Después de medianoche`, precio: 2800},
+     {nombre: `Cementerio de animales`, precio: 3500},
+     {nombre: `El bazar de los malos sueños`, precio: 3500},
+     {nombre: `Quien pierde, paga`, precio: 3000},
+     {nombre: `La tormenta del siglo`, precio: 4000},
+     {nombre: `Rabia`, precio: 2500},
+     {nombre: `Misery`, precio: 3000},
+     {nombre: `Joyland`, precio: 3000},
+     {nombre: `La mitad oscura`, precio: 4500},
+     {nombre: `El instituto`, precio: 3600},     
+     {nombre: `Christine`, precio: 2000}  
+    ]; 
+    libros.sort( function(v1, v2){return v1[1]>v2[1]?-1:1});         
+    console.table(libros);
+let precio= Number(prompt("Ingrese una cifra el dinero"));
+let nombreL = libros.filter(elemento => elemento.precio <= precio);
+console.log(nombreL)
+//.toUpperCase(); console.log(libros.includes["Rabia"])
+//¿cómo puedo hacer que el nombreL tenga un .sort así me salen los resultados en orden?
 
