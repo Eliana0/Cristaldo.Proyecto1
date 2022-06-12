5fe38030af634499ecc626c91fd0a9f4f21f0894
Eliana <elianacristaldoo@gmail.com>
2022-06-11 22:27:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// class Libro{
+//     constructor(titulo, precio){
+//         this.titulo= titulo;
+//         this.precio= precio;
+//     }
+//     descuento(){
+//         if((this.precio >=3000)&&(this.precio < 4000)){
+//             alert(`Su libro tiene un descuento del 10% precio final es: `+ Math.round(this.precio/(1.1)));
+//         }if((this.precio >=4000)&&(this.precio < 5000)){
+//             alert(`Su libro tiene un descuento del 20% precio final es: `+ Math.round(this.precio/(1.2)));
+//         }if(this.precio >=5000){
+//             alert(`Su libro tiene un descuento del 30% precio final es: `+ Math.round(this.precio/(1.3)));
+//         }
+//     }
+// }
+
+// const it= new Libro(`It`, 6400);
+// const despues= new Libro(`Después`, 4200); 
+// const elResplandor= new Libro(`El Resplandor`, 3600);
+// const laCupula= new Libro(`La Cúpula`, 6500);
+// const laExpedicion= new Libro(`La Expedición`, 3600);
+// const ojosDeFuego= new Libro(`Ojos de fuego`, 4200);
+// const desesperacion= new Libro(`Desesperación`, 3700);
+// const elVisitante= new Libro(`El Visitante`, 4700);
+// const laMillaVerde= new Libro(`la milla verde`, 3500);
+// const despuesDeMedianoche= new Libro(`Después de medianoche`, 2800);
+// const cementerioDeAnimales= new Libro(`Cementerio de animales`, 3500);
+// const elBazarDeLosMalosSueños= new Libro(`El bazar de los malos sueños`, 3500);
+// const quienPierdePaga= new Libro(`Quien pierde, paga`, 3000);
+// const laTormentaDelSiglo= new Libro(`La tormenta del siglo`, 4000);
+// const rabia= new Libro(`Rabia`, 2500);
+// const misery= new Libro(`Misery`, 3000);
+// const joyland= new Libro(`Joyland`, 3000);
+// const laMitadOscura= new Libro(`La mitad oscura`, 4500);
+// const elInstituto= new Libro(`El instituto`, 3600);
+// const christine= new Libro(`Christine`, 2000);
+
+// console.log(ojosDeFuego.descuento())
+
+
+
+
+
+
+
+//  var libros = new Array;
+//  libros = [
+//     [`It`, `$6.400`],
+//     [`Después`, `$4.200`], 
+//     [`El Resplandor`, `$3.600`], 
+//     [`La Cúpula`, `$6.500`],
+//     [`La Expedición`, `$3.600`],
+//     [`Ojos de fuego`, `$4.200`],
+//     [`Desesperación`, `$3.700`],
+//     [`El Visitante`, `$4.700`],
+//     [`la milla verde`, `$3.500`],
+//     [`Después de medianoche`, `$2.800`],
+//     [`Cementerio de animales`, `$3.500`],
+//     [`El bazar de los malos sueños`, `$3.500`],
+//     [`Quien pierde, paga`, `$3.000`],
+//     [`La tormenta del siglo`, `$4.000`],
+//     [`Rabia`, `$2.500`],
+//     [`Misery`, `$3.000`],
+//     [`Joyland`, `$3.000`],
+//     [`La mitad oscura`, `$4.500`],
+//     [`El instituto`, `$3.600`],
+//     [`Christine`, `$2.000`]
+//  ];
+// libros.sort( function(v1, v2){return v1[1]>v2[1]?-1:1});        
+// console.table(libros);
+
+//let nombreL = prompt("Escribe el nombre del libro")//.toUpperCase();
+// console.log(libros.includes["Rabia"])
+
+//¿cómo puedo buscar una palabra especifica siendo un array dentro de otro?
+
