dfd3f949b9b803d3b778d4a85945b3fd3aafbb0a
Eliana <elianacristaldoo@gmail.com>
2022-07-04 16:10:54 -0300

    agrega algunos operadores avanzados

--- index.js
+++ index.js
@@ -23,15 +23,11 @@ const libros = [
   {id: 20, nombre: `CHRISTINE`, precio: 2000, genero: "TERROR", img: "ima/CHRISTINE.jpg", descripcion: "Arnie Cunningham compra su primer auto. Pero, cuando la gente comienza a morir en sospechosos accidentes, no se puede negar la verdad: el auto está vivo."} 
 ];
 
-
-/* localStorage.setItem("libros", JSON.stringify(libros));  */
-/* let libros= JSON.parse(localStorage.getItem("libros")); */
-libros.sort((v1, v2) => v1.precio - v2.precio);         
+libros.sort((v1, v2) => v1.precio - v2.precio);  
 console.table(libros);
 
 
 let carrito = [];
-let dueño = "";
 let n10= "10";
 let n20= "20";
 let n30= "30";
@@ -39,15 +35,9 @@ let p10= 0.1;
 let p20= 0.2;
 let p30= 0.3;
 const carritoDeCompras = document.querySelector(`#carritoDeCompras`);
+const vaciarCarrito = document.getElementById(`vaciarCarrito`);
 const total = document.querySelector(`#total`);
-const mainFloor= document.getElementById("mainFloor");
-const agregar= document.getElementById("agregar");
-const sustraer= document.getElementById("sustraer");
-
-
-
-
-
+let botonOrdenar= document.getElementById("ordenar");
 
 
 
@@ -91,7 +81,7 @@ function crearTienda(array){
 
         total.innerHTML= ``;
 
-      let sumar = carrito.reduce((acc, e) => acc + e.precio, 0);
+      let sumar = carrito.reduce((acc, e) => acc + e?.precio, 0);
       let valor = document.createElement("div");
 
       valor.innerHTML= `<div>Precio</div><div>$${sumar}</div>`;
@@ -120,15 +110,15 @@ function crearTienda(array){
 
 
 
-  function vaciarCarrito() {
-    let vaciarCarrito = document.getElementById(`vaciarCarrito`)
-    vaciarCarrito.className= "botonFinalizar"
+  function borrarCarrito() {
+    
+    vaciarCarrito.className= "botonFinalizar";
     vaciarCarrito.addEventListener(`click`, ()=>{ 
       carrito = [];
       total.innerHTML= ``;
       carritoDeCompras.innerHTML= ``;
     })
-}vaciarCarrito()
+}borrarCarrito()
 
 
 
@@ -143,14 +133,15 @@ function crearTienda(array){
       const susMail= document.getElementById("susMail").value.toLowerCase();
   
        e.preventDefault();
-        if(susNombre.length == 0){
-          susText.innerHTML= `*Escriba su nombre`
-        }else if(susMail.length == 0){
-          susText.innerHTML= `*Escriba su mail`
-        }else{
 
-          function guardarStorage(){
-            let suscriptor= {
+         if(susNombre.length == 0){
+           susText.innerHTML= `*Escriba su nombre`
+         }else if(susApellido.length == 0){
+             susText.innerHTML= `*Escriba su apellido`
+         }else if(susMail.length == 0){
+            susText.innerHTML= `*Escriba su mail`
+         }else{
+            const suscriptor= {
               nombre: susNombre,
               apellido: susApellido,
               mail: susMail,
@@ -158,81 +149,53 @@ function crearTienda(array){
             };
             
             localStorage.setItem("suscriptor", JSON.stringify(suscriptor));
-          }
-          guardarStorage()
+            
+            alert(`¡¡Felisitaciones!! ${susNombre} Te has suscripto a la página de libros de Stephen king`);
+            susNombre.innerHTML= "";
+            susApellido.innerHTML= "";
+            susMail.innerHTML= ""; 
         }
-        alert(`¡¡Felisitaciones!! ${susNombre} Te has suscripto a la página de libros de Stephen king`) 
+      
       })
     }
-    //)
-    // }
     suscribirse()  
+
+
 
 
     function verDatosSuscriptor(){
       
       const botonUsuario= document.getElementById("botonUsuario");
       botonUsuario.addEventListener("click", ()=>{
+        total.innerHTML="";
         
         let mail= document.getElementById('buscarUsuario').value.toLowerCase();
-        let suscriptor= JSON.parse(localStorage.getItem(`suscriptor`));
-        if(suscriptor.mail === mail){
-          let nombre= suscriptor.nombre;
-          let apellido= suscriptor.apellido;
-          let mail= suscriptor.mail;
-          let productos= suscriptor.productos;
-          for(let libro of productos){
-            agregaCompras(libro)
-          }
-          let mensajeSus= document.getElementById("mensajeSus");
-          mensajeSus.innerHTML="";
-          mensajeSus.innerHTML=`<h2>Hola, ${nombre} ${apellido}, bienvenido nuevamente<h2>`;
-          
-        } else{
-          alert("Usuario no suscripto")
-        }
+      let suscriptor= JSON.parse(localStorage.getItem(`suscriptor`));
+        suscriptor.mail === mail ? bienvenidaUsuario() : alert("Usuario no suscripto");
       })
-      
     }verDatosSuscriptor()
+
+
+
+
     function cerrarUsuario(){
         const cerrarSesion = document.getElementById("cerrarSesion");
         cerrarSesion.addEventListener("click", ()=>{
+
         let suscriptor= JSON.parse(localStorage.getItem(`suscriptor`));
-        let nombre= suscriptor.nombre;
-        let apellido= suscriptor.apellido;
-        let mail= suscriptor.mail;
-        let productos= suscriptor.productos;
+        let {nombre, apellido} = suscriptor;
         let mensajeSus= document.getElementById("mensajeSus");
         mensajeSus.innerHTML="";
+
         alert(`Hasta la proxima, ${nombre} ${apellido}`);
 
-        suscriptor.productos= carrito;
-        localStorage.setItem("suscriptor", JSON.stringify(suscriptor))
+        suscriptor.productos = carrito;
+        localStorage.setItem("suscriptor", JSON.stringify(suscriptor));
         carrito = [];
         total.innerHTML= ``;
         carritoDeCompras.innerHTML= ``;
       })
     }cerrarUsuario()
-
-
-
-//Recuperar datos previos
-/* let usuario;
-let use= JSON.stringify(localStorage.getItem(`usuario`))
-//Si había algo almacenado, lo recupero. Si no, le paso un ingreso
-if (use){
-  usuario = use
-}else{
-  usuario = prompt(`ingrese su nombre de usuairo`)
-}
-//Recuparar datos previos
-carrito= []
-let carri= JSON.stringify(localStorage.getItem(`carrito`))
-//inicializo mi carrito con un array vacio o con el registro que haya quedado en LS
-if(carri){
-  carrito = carri
-} */
-//Funicon que redendiza el carrito
 
 
 
@@ -258,6 +221,11 @@ function contacto(){
       else{
         textContacto.innerHTML=`Su mensaje ha sido enviado correctamente. A la brevedad nos pondremos en contacto con usted`
       }
+      formNombre.innerHTML= "";
+      formApellido.innerHTML= "";
+      formMail.innerHTML= "";
+      formNumero.innerHTML= "";
+      formText.innerHTML= "";
     })
     }
 contacto() 
@@ -265,155 +233,12 @@ contacto()
 
 
 
- function modificar(){
-let botonModificar= document.getElementById("botonModificar");
-botonModificar.addEventListener("click", ()=>{
-
-    mainFloor.innerHTML="";
-
-const password= document.getElementById('password');
-if (password.value === dueño){
-
-  let contenedorModificar= document.createElement("div");
-  contenedorModificar.className= "contenedorModificar";
-
-    let agregarElemento= document.createElement("button");
-    agregarElemento.className="botonFinalizar";
-    agregarElemento.innerHTML= 'Agregar libro';
-    agregarElemento.addEventListener("click", ()=>{
-      addLibro()
-    })
-  
-    let sustraerElemento= document.createElement("button");
-    sustraerElemento.className="botonFinalizar"
-    sustraerElemento.innerHTML= 'Quitar libro';
-    sustraerElemento.addEventListener("click", ()=>{
-      sustraerLibro()
-    })
-    
-    contenedorModificar.appendChild(agregarElemento);
-    contenedorModificar.appendChild(sustraerElemento);
-    mainFloor.appendChild(contenedorModificar);
-
-    botonModificar.addEventListener("click", ()=>{
-      mainFloor.innerHTML="";
-      modificar()
-    })
-}
- })
-}
-modificar()
- 
-
-
-
-function addLibro() {
-
-  agregar.innerHTML="";
-
-let botonBorrar=  document.createElement("button");
-let newLibro= document.createElement("div");
-let botonPush= document.createElement("button");
-let contentFinalizar= document.createElement("div");
-
-contentFinalizar.className="contentFinalizar";
-botonBorrar.className="botonBorrar";
-botonBorrar.innerHTML= `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-</svg>`;
-newLibro.innerHTML= `
-<div class="divContraseña"><input type="text" class="password" placeholder="Título" id="newLibroNombre"></div>
-<div class="divContraseña"><input type="number" class="password" placeholder="Precio" id="newLibroPrecio"></div>
-<div class="divContraseña"><input type="text" class="password" placeholder="Genero" id="newLibroGenero"></div>
-<div class="divContraseña"><input type="text" class="password" placeholder="Imágen" id="newLibroImg"></div>
-<div class="divContraseña"> <textarea class="password" placeholder="Descripción" id="newLibroDescripcion" style="height: 100px"></textarea></div>
-`;
-
-botonPush.innerText= `Guardar`;
-botonPush.className= "botonFinalizar";
-botonPush.addEventListener("click", ()=>{
-  let newLibroNombre= document.getElementById("newLibroNombre").value.toUpperCase();
-  let newLibroPrecio= document.getElementById("newLibroPrecio").value;
-  let newLibroGenero= document.getElementById("newLibroGenero").value.toUpperCase();
-  let newLibroImg= document.getElementById("newLibroImg").value;
-  let newLibroDescripcion= document.getElementById("newLibroDescripcion").value;
-
-    let libros= JSON.parse(localStorage.getItem("libros"));
-  libros.push({nombre: newLibroNombre, precio: newLibroPrecio, genero: newLibroGenero, img: newLibroImg, descripcion: newLibroDescripcion});
-  localStorage.setItem("libros", JSON.stringify(libros));
-
-  agregar.innerHTML= '';
-  alert("El libro se ha agregado correctamente")
-})
-
-botonBorrar.addEventListener("click", ()=>{
-  agregar.innerHTML= '';
-})
-
-contentFinalizar.append(botonPush)
-agregar.appendChild(botonBorrar)
-agregar.appendChild(newLibro)
-agregar.appendChild(contentFinalizar)
-}
-
-
-
-
-
-function sustraerLibro(){
-
-  sustraer.innerHTML="";
-      
-let botonBorrar= document.createElement("button");
-let titulo= document.createElement("div");
-let input= document.createElement("div");
-let botonSustraer= document.createElement("button");
-let contentFinalizar= document.createElement("div");
-
-contentFinalizar.className="contentFinalizar";
-botonBorrar.className= "botonBorrar";
-botonBorrar.innerHTML= `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-</svg>`;
-titulo.innerHTML= '<p>Escriba el libro que desea sustraer</p>';
-titulo.className= "subtext";
-input.innerHTML= `<div class="divContraseña"><input type="text" placeholder="Título" class="password" id="libroASustraer"></div>`;
-botonSustraer.innerHTML= `Eliminar`;
-botonSustraer.className="botonFinalizar";
-
-botonSustraer.addEventListener("click", ()=>{
-  let libros= JSON.parse(localStorage.getItem("libros"));
-  let libroASustraer= document.getElementById("libroASustraer").value.toUpperCase();
-  let buscarSustraer= libros.find(elemento => elemento.nombre == libroASustraer);
-  let numBuscar= libros.indexOf(buscarSustraer);
-  
-  libros.splice(numBuscar ,1);
-  localStorage.setItem("libros", JSON.stringify(libros));
-
-  sustraer.innerHTML=``;
-  alert('El libro se ha eliminado correctamente')
-})
-
-botonBorrar.addEventListener("click", ()=>{
-  sustraer.innerHTML=``;
-
-})
-
-contentFinalizar.append(botonSustraer);
-sustraer.appendChild(botonBorrar);
-sustraer.appendChild(titulo);
-sustraer.appendChild(input);
-sustraer.appendChild(contentFinalizar);
-}
-
-
 function agregaCompras(objeto){
 
-  let carritoDeCompras= document.getElementById("carritoDeCompras");
   let carritoElement= document.createElement("div");
   let borrarElement= document.createElement("button");
   carritoElement.className= "carritoElement"
-  carritoElement.innerHTML= `<div>${objeto.nombre}</div><div>$${objeto.precio}</div>`
+  carritoElement.innerHTML= `<div>${objeto?.nombre}</div><div>$${objeto?.precio}</div>`
   borrarElement.className="borrar";
   borrarElement.innerHTML= `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
@@ -422,8 +247,8 @@ function agregaCompras(objeto){
   borrarElement.addEventListener("click", ()=>{
     carritoElement.parentNode.removeChild(carritoElement)
     
-    let nombre= objeto.nombre;
-    let buscarSustraer= carrito.find(libro => libro.nombre == nombre);
+    let nombre= objeto?.nombre;
+    let buscarSustraer= carrito.find(elemento => elemento?.nombre == nombre);
     let numBuscar= carrito.indexOf(buscarSustraer);
     carrito.splice(numBuscar ,1);
     
@@ -432,7 +257,7 @@ function agregaCompras(objeto){
   carritoElement.appendChild(borrarElement);
   carritoDeCompras.append(carritoElement); 
   
-  carrito.push({precio: objeto.precio, nombre: objeto.nombre})
+  carrito.push({precio: objeto?.precio, nombre: objeto?.nombre})
 }
    
 
@@ -454,3 +279,16 @@ function porcentaje(n, porcentaje, sumar){
   total.appendChild(cardFloor);
 }
 
+
+
+function bienvenidaUsuario(){
+  let suscriptor= JSON.parse(localStorage.getItem(`suscriptor`));
+  let {nombre, apellido, productos} = suscriptor;
+  
+  for(let libro of productos){
+    agregaCompras(libro)
+  }
+  let mensajeSus= document.getElementById("mensajeSus");
+  mensajeSus.innerHTML="";
+  mensajeSus.innerHTML=`<h2>Hola, ${nombre} ${apellido}, bienvenido nuevamente<h2>`;
+}
